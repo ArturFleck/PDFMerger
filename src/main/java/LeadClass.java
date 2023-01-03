cc912b477406581ee8e63b66a10721f29d9199de
Artur Fleck <artur.ivanishyn@gmail>
2023-01-03 15:31:27 +0200

    code refactor

--- src/main/java/LeadClass.java
+++ src/main/java/LeadClass.java
@@ -5,13 +5,13 @@ public class LeadClass {
     public static void main(String[] args) throws IOException {
         File pathFromExport = new File("C:/Test5/3D-CON~1/KM5/Export/");
         File pathFromTMP = new File("C:/Elecran/3D-Constructor 7/KM5/TMP/");
-        String pathTo = "D:/_profile/Desktop/Типові меблі ЕЛКОН ДІЗАЙН/ТИПОВІ КУХНІ/основа верх/МВ 96/";
-        File pathToMove = new File("D:/_profile/Desktop/Типові меблі ЕЛКОН ДІЗАЙН/ТИПОВІ КУХНІ/основа верх/ДОП/");
+        String pathTo = "D:/_profile/Desktop/Типові меблі ЕЛКОН ДІЗАЙН/ТИПОВІ КУХНІ/основа верх/МВ 36/";
+        File pathToMove = new File("D:/_profile/Desktop/Типові меблі ЕЛКОН ДІЗАЙН/ТИПОВІ КУХНІ/основа верх/МВ 36/");
 
-        //FileManager.excelMover(pathFromExport, pathTo);
-        //FileManager.projectMover(pathFromTMP,pathTo);
-        //PDFMerger.directories(pathFromTMP, pathTo);
-        FileMover.dwgMover(pathToMove);
+        FileManager.excelMover(pathFromExport, pathTo);
+        FileManager.projectMover(pathFromTMP,pathTo);
+        PDFMerger.mergePDF(pathFromTMP, pathTo);
+        //FileMover.dwgMover(pathToMove);
         FileMover.mprMover(pathToMove);
 
     }
--- src/main/java/PDFMerger.java
+++ src/main/java/PDFMerger.java
@@ -7,19 +7,20 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 public class PDFMerger {
-    public static void directories(File pathFrom, String pathTo) throws IOException {
+    public static void mergePDF(File pathFrom, String pathTo) throws IOException {
         String[] listOfFolders = pathFrom.list();
 
         assert listOfFolders != null;
         for (String dir : listOfFolders) {
             String files = pathFrom + "/" + dir;
-            filesForMerging(files);
+            filesForMerging2(files);
 
-            String dest = pathTo + dir + "/" + dir + ".pdf";
-            String dest2 = pathTo + dir + "/" + "Креслення PDF/" + dir + ".pdf";
+            String dest = pathTo + dir + "/Ескізи для менеджера/" + dir + ".pdf";
+            String dest2 = pathTo + dir + "/Креслення PDF/" + dir + ".pdf";
             String name = files + ".pdf";
 
-            Files.copy(Path.of(name), Path.of(dest), StandardCopyOption.REPLACE_EXISTING);
+            Files.move(Path.of(name), Path.of(dest), StandardCopyOption.REPLACE_EXISTING);
+            filesForMerging(files);
             Files.move(Path.of(name), Path.of(dest2), StandardCopyOption.REPLACE_EXISTING);
 
             System.out.println(files);
@@ -37,6 +38,24 @@ public class PDFMerger {
         for (File file : listOfFiles) {
             if (file.isFile()) {
                 ut.addSource(path + "/" + file.getName());
+            }
+        }
+
+        ut.setDestinationFileName(path + ".pdf");
+        ut.mergeDocuments();
+        System.out.println("Created  " + path + ".pdf");
+    }
+
+    public static void filesForMerging2(String path) throws IOException {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        PDFMergerUtility ut = new PDFMergerUtility();
+
+        assert listOfFiles != null;
+        for (int i = 1; i < listOfFiles.length; i++) {
+            if ((listOfFiles[i]).isFile()) {
+                ut.addSource(path + "/" + (listOfFiles[i]).getName());
             }
         }
 
